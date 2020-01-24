439d6bf4681eafda09ceb96597aad9bdea5e0487
szk333 <Margonem11>
2020-01-24 19:05:57 +0100

    better scheduled

--- users/utils.py
+++ users/utils.py
@@ -4,5 +4,5 @@ from datetime import datetime, timedelta
 def next_time_interval(date):
     if type(date) == str:
         date = datetime.strptime(date, '%Y-%m-%dT%H:%M')
-    return date + (datetime.min - date) % timedelta(minutes=5)
+    return date + (datetime.min - date.replace(tzinfo=None)) % timedelta(minutes=5)
 
