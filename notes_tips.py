bb8f6d6164c654c3472432f0fad55531559736b7
Adam Sebastian Górski <a.s.gorski@stud.elka.pw.edu.pl>
2020-01-27 10:43:41 +0100

    Add new file

---
+++ notes_tips.py
@@ -0,0 +1,19 @@
+przedstawienie problemu - ludzie etc
+jak ten problem rozwiązują inni
+analiza, szukanie rozwiązania ew kierowanie - ankieta
+badania, wywiady
+dopiero potem, nasze pomysły
+potem rozwiązanie
+opis funkcjonalny
+
+
+opis techniczny docelowego rozwiązania
+schematy, moduły
+bez głębokich szczegółów inż opr.
+prototyp - jak uprościliśmy w celu prototypowania
+opis etap testów, scenariusze testowe
+oraz testy użyteczności rozwiązania
+wnioski, dodane, pomysły na przyszłość
+podsumowanie
+
+podział zadan organizacja pracy
